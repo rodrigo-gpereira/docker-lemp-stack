1998fa0c8082d56d21333158d75d39c91294bf41
Rodrigo Gomes <rodrigo@infografic.com.br>
2021-05-04 19:27:58 -0300

    Add Mailhog Container

--- cli/site-delete.sh
+++ cli/site-delete.sh
@@ -16,6 +16,7 @@ die() { echo -e '\e[1;31m'$1'\e[m'; exit 1; }
 docker stop ${1//[-._]/}_php_1
 docker stop ${1//[-._]/}_nginx_1
 docker stop ${1//[-._]/}_mysql_1
+docker stop ${1//[-._]/}_mailhog_1
 
 #remover o Arquivos hosts do terminal local
 cli/setup-hosts-file.sh $1 r
@@ -26,6 +27,7 @@ rm -r ../$1
 docker rm -v ${1//[-._]/}_php_1
 docker rm -v ${1//[-._]/}_nginx_1
 docker rm -v ${1//[-._]/}_mysql_1
+docker rm -v ${1//[-._]/}_mailhog_1
 
 docker network rm ${1//[-._]/}_dev-network
 
